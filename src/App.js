030b8076056be800b0f588489c9a514d72869c76
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-01-28 23:29:46 -0400

    Add styling to header

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import './style/App.css';
 
 function App() {
     const activeStyle = {
         color: 'red',
+        borderBottom: 'solid 2px red',
     };
 
     return (
         <div className="App">
-            <h1>Hello from App</h1>
-            <nav>
-                <NavLink
-                    to="/"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }
-                >
-                    Home
-                </NavLink>
-                <br />
-                <NavLink
-                    to="/shop"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }
-                >
-                    Shop
-                </NavLink>
-            </nav>
+            <header>
+                <h1>Hello from App</h1>
+                <nav>
+                    <NavLink
+                        to="/"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        to="/shop"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }
+                    >
+                        Shop
+                    </NavLink>
+                </nav>
+            </header>
             <div>
                 <Outlet />
             </div>
