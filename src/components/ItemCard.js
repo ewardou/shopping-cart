cd4a7bdf407094cf710a3818ba8c7483f88544b6
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-01-30 21:52:11 -0400

    Add content to shop section

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../style/ItemCard.css';
 
 export default function ItemCard(props) {
     return (
-        <div>
-            <img src={props.image} alt={props.model} />
-            <p>{props.model}</p>
-            <p>{`Price: $${props.price} `}</p>
-            <Link to={`${props.itemID}`}>Buy</Link>
+        <div className="card">
+            <Link to={`${props.itemID}`}>
+                <img src={props.image} alt={props.model} />
+                <p>{props.model}</p>
+                <p>{`Price: $${props.price} `}</p>
+            </Link>
         </div>
     );
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import ItemCard from './ItemCard';
-import fast from '../images/shoes/NB5740.webp';
 import { info } from '../shoesInfo';
+import '../style/Shop.css';
+import { useState } from 'react';
 
 export function Shop() {
+    const [shoes, setShoes] = useState(info);
+
+    async function filterArray(e) {
+        const brand = e.target.textContent;
+        await resetArray();
+        setShoes((prev) => {
+            return prev.filter((item) => item.brand === brand);
+        });
+    }
+
+    async function resetArray() {
+        return Promise.resolve(setShoes(info));
+    }
+
+    const array = shoes.map((item) => {
+        return (
+            <ItemCard
+                itemID={item.itemID}
+                model={item.model}
+                price={item.price}
+                image={item.image}
+                key={item.itemID}
+            />
+        );
+    });
     return (
-        <div>
-            <h1>Hello from shop</h1>
-            <ItemCard itemID={'item4'} model={'test model'} image={fast} />
+        <div className="shop">
+            <div>
+                <aside>
+                    <p>Filter by brand</p>
+                    <button onClick={resetArray}>All</button>
+                    <button onClick={filterArray}>Adidas</button>
+                    <button onClick={filterArray}>Puma</button>
+                    <button onClick={filterArray}>New Balance</button>
+                    <button onClick={filterArray}>Nike</button>
+                    <button onClick={filterArray}>Reebok</button>
+                </aside>
+            </div>
+            <div>{array}</div>
         </div>
     );
 }
