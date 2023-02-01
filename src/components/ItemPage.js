d83ddcf8d6cd5b7c9de73e7b7121a2ebb1d3b080
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-02-01 16:47:13 -0400

    Add ItemPage component

--- src/components/ItemPage.js
+++ src/components/ItemPage.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import '../style/ItemPage.css';
+import { Link } from 'react-router-dom';
+import { info } from '../shoesInfo';
+import { useState } from 'react';
+
+function Gallery({ item, onClick }) {
+    return (
+        <div className="gallery">
+            <img src={item.image} alt={item.model} onClick={onClick} />
+            <img src={item.secondImage} alt={item.model} onClick={onClick} />
+            <img src={item.thirdImage} alt={item.model} onClick={onClick} />
+            <img src={item.fourthImage} alt={item.model} onClick={onClick} />
+        </div>
+    );
+}
 
 export default function ItemPage() {
     const { itemID } = useParams();
-    return <h1>{itemID} information</h1>;
+    const [item] = info.filter((item) => {
+        return item.itemID === itemID;
+    });
+    const [image, setImage] = useState(item.image);
+    function changeImage(e) {
+        setImage(e.target.src);
+    }
+
+    return (
+        <div className="item-page">
+            <Link to="/shop">Go back</Link>
+            <div>
+                <img src={image} alt={item.model} />
+            </div>
+            <div>
+                <h1>{item.model}</h1>
+                <p>{`$${item.price}`}</p>
+                <Gallery item={item} onClick={changeImage} />
+                <p>
+                    <label htmlFor="size">Size: </label>
+                    <select id="size">
+                        <option>7</option>
+                        <option>7.5</option>
+                        <option>8</option>
+                        <option>8.5</option>
+                        <option>9</option>
+                        <option>9.5</option>
+                        <option>10</option>
+                        <option>10.5</option>
+                        <option>11</option>
+                        <option>11.5</option>
+                        <option>12</option>
+                        <option>12.5</option>
+                    </select>
+                </p>
+                <button>Add to cart</button>
+            </div>
+        </div>
+    );
 }
--- src/shoesInfo.js
+++ src/shoesInfo.js
@@ -1,33 +1,77 @@
 import adidasDON from './images/shoes/ADIDASDON.webp';
 import adidasDON2 from './images/shoes/ADIDASDON-2.webp';
+import adidasDON3 from './images/shoes/ADIDASDON-3.webp';
+import adidasDON4 from './images/shoes/ADIDASDON-4.webp';
+
 import adidasNMD from './images/shoes/ADIDASNMD.webp';
 import adidasNMD2 from './images/shoes/ADIDASNMD-2.webp';
+import adidasNMD3 from './images/shoes/ADIDASNMD-3.webp';
+import adidasNMD4 from './images/shoes/ADIDASNMD-4.webp';
+
 import adidasOzelia from './images/shoes/ADIDASOZELIA.webp';
 import adidasOzelia2 from './images/shoes/ADIDASOZELIA-2.webp';
+import adidasOzelia3 from './images/shoes/ADIDASOZELIA-3.webp';
+import adidasOzelia4 from './images/shoes/ADIDASOZELIA-4.webp';
+
 import adidasTen from './images/shoes/ADIDASTOPTEN.webp';
 import adidasTen2 from './images/shoes/ADIDASTOPTEN-2.webp';
+import adidasTen3 from './images/shoes/ADIDASTOPTEN-3.webp';
+import adidasTen4 from './images/shoes/ADIDASTOPTEN-4.webp';
+
 import adidasTrae from './images/shoes/ADIDASTRAE.webp';
 import adidasTrae2 from './images/shoes/ADIDASTRAE-2.webp';
+import adidasTrae3 from './images/shoes/ADIDASTRAE-3.webp';
+import adidasTrae4 from './images/shoes/ADIDASTRAE-4.webp';
+
 import newBalance from './images/shoes/NB5740.webp';
 import newBalance2 from './images/shoes/NB5740-2.webp';
+import newBalance3 from './images/shoes/NB5740-3.webp';
+import newBalance4 from './images/shoes/NB5740-4.webp';
+
 import pumaFuture from './images/shoes/PUMAFUTURERIDER.webp';
 import pumaFuture2 from './images/shoes/PUMAFUTURERIDER-2.webp';
+import pumaFuture3 from './images/shoes/PUMAFUTURERIDER-3.webp';
+import pumaFuture4 from './images/shoes/PUMAFUTURERIDER-4.webp';
+
 import pumaMB from './images/shoes/PUMAMB1LO.webp';
 import pumaMB2 from './images/shoes/PUMAMB1LO-2.webp';
+import pumaMB3 from './images/shoes/PUMAMB1LO-3.webp';
+import pumaMB4 from './images/shoes/PUMAMB1LO-4.webp';
+
 import pumaRS from './images/shoes/PUMARS-X.webp';
 import pumaRS2 from './images/shoes/PUMARS-X-2.webp';
+import pumaRS3 from './images/shoes/PUMARS-X-3.webp';
+import pumaRS4 from './images/shoes/PUMARS-X-4.webp';
+
 import reebokClassic from './images/shoes/RBCLASSICLEATHER.webp';
 import reebokClassic2 from './images/shoes/RBCLASSICLEATHER-2.webp';
+import reebokClassic3 from './images/shoes/RBCLASSICLEATHER-3.webp';
+import reebokClassic4 from './images/shoes/RBCLASSICLEATHER-4.webp';
+
 import reebokClub from './images/shoes/RBCLUBC.webp';
 import reebokClub2 from './images/shoes/RBCLUBC-2.webp';
+import reebokClub3 from './images/shoes/RBCLUBC-3.webp';
+import reebokClub4 from './images/shoes/RBCLUBC-4.webp';
+
 import nikeMetcon from './images/shoes/NIKEMETCON.webp';
 import nikeMetcon2 from './images/shoes/NIKEMETCON-2.webp';
+import nikeMetcon3 from './images/shoes/NIKEMETCON-3.webp';
+import nikeMetcon4 from './images/shoes/NIKEMETCON-4.webp';
+
 import nikeAir from './images/shoes/NIKEAIR.webp';
 import nikeAir2 from './images/shoes/NIKEAIR-2.webp';
+import nikeAir3 from './images/shoes/NIKEAIR-3.webp';
+import nikeAir4 from './images/shoes/NIKEAIR-4.webp';
+
 import nikeBlazer from './images/shoes/NIKEBLAZER.webp';
 import nikeBlazer2 from './images/shoes/NIKEBLAZER-2.webp';
+import nikeBlazer3 from './images/shoes/NIKEBLAZER-3.webp';
+import nikeBlazer4 from './images/shoes/NIKEBLAZER-4.webp';
+
 import nikeGiannis from './images/shoes/NIKEGIANNIS.webp';
 import nikeGiannis2 from './images/shoes/NIKEGIANNIS-2.webp';
+import nikeGiannis3 from './images/shoes/NIKEGIANNIS-3.webp';
+import nikeGiannis4 from './images/shoes/NIKEGIANNIS-4.webp';
 
 export const info = [
     {
@@ -35,6 +79,8 @@ export const info = [
         model: 'Adidas D.O.N Issue 3',
         image: adidasDON,
         secondImage: adidasDON2,
+        thirdImage: adidasDON3,
+        fourthImage: adidasDON4,
         itemID: 'item1',
         brand: 'Adidas',
     },
@@ -43,6 +89,8 @@ export const info = [
         model: 'Adidas NMD V2',
         image: adidasNMD,
         secondImage: adidasNMD2,
+        thirdImage: adidasNMD3,
+        fourthImage: adidasNMD4,
         itemID: 'item2',
         brand: 'Adidas',
     },
@@ -51,6 +99,8 @@ export const info = [
         model: 'Adidas Originals Ozelia',
         image: adidasOzelia,
         secondImage: adidasOzelia2,
+        thirdImage: adidasOzelia3,
+        fourthImage: adidasOzelia4,
         itemID: 'item3',
         brand: 'Adidas',
     },
@@ -59,6 +109,8 @@ export const info = [
         model: 'Adidas Originals Top Ten RB',
         image: adidasTen,
         secondImage: adidasTen2,
+        thirdImage: adidasTen3,
+        fourthImage: adidasTen4,
         itemID: 'item4',
         brand: 'Adidas',
     },
@@ -67,6 +119,8 @@ export const info = [
         model: 'Adidas Trae Young 1',
         image: adidasTrae,
         secondImage: adidasTrae2,
+        thirdImage: adidasTrae3,
+        fourthImage: adidasTrae4,
         itemID: 'item5',
         brand: 'Adidas',
     },
@@ -75,6 +129,8 @@ export const info = [
         model: 'New Balance 5740 V1',
         image: newBalance,
         secondImage: newBalance2,
+        thirdImage: newBalance3,
+        fourthImage: newBalance4,
         itemID: 'item6',
         brand: 'New Balance',
     },
@@ -83,6 +139,8 @@ export const info = [
         model: 'Puma Future Rider',
         image: pumaFuture,
         secondImage: pumaFuture2,
+        thirdImage: pumaFuture3,
+        fourthImage: pumaFuture4,
         itemID: 'item7',
         brand: 'Puma',
     },
@@ -91,6 +149,8 @@ export const info = [
         model: 'Puma MB1 LO Teambank',
         image: pumaMB,
         secondImage: pumaMB2,
+        thirdImage: pumaMB3,
+        fourthImage: pumaMB4,
         itemID: 'item8',
         brand: 'Puma',
     },
@@ -99,6 +159,8 @@ export const info = [
         model: 'Puma RS-X',
         image: pumaRS,
         secondImage: pumaRS2,
+        thirdImage: pumaRS3,
+        fourthImage: pumaRS4,
         itemID: 'item9',
         brand: 'Puma',
     },
@@ -107,6 +169,8 @@ export const info = [
         model: 'Reebok Classic Leather Speckle',
         image: reebokClassic,
         secondImage: reebokClassic2,
+        thirdImage: reebokClassic3,
+        fourthImage: reebokClassic4,
         itemID: 'item10',
         brand: 'Reebok',
     },
@@ -115,6 +179,8 @@ export const info = [
         model: 'Reebok Club C Vintage',
         image: reebokClub,
         secondImage: reebokClub2,
+        thirdImage: reebokClub3,
+        fourthImage: reebokClub4,
         itemID: 'item11',
         brand: 'Reebok',
     },
@@ -123,6 +189,8 @@ export const info = [
         model: 'Nike Metcon 8',
         image: nikeMetcon,
         secondImage: nikeMetcon2,
+        thirdImage: nikeMetcon3,
+        fourthImage: nikeMetcon4,
         itemID: 'item12',
         brand: 'Nike',
     },
@@ -131,6 +199,8 @@ export const info = [
         model: "Nike Air Max '97",
         image: nikeAir,
         secondImage: nikeAir2,
+        thirdImage: nikeAir3,
+        fourthImage: nikeAir4,
         itemID: 'item13',
         brand: 'Nike',
     },
@@ -139,6 +209,8 @@ export const info = [
         model: 'Nike Blazer Mid 77 Jumbo',
         image: nikeBlazer,
         secondImage: nikeBlazer2,
+        thirdImage: nikeBlazer3,
+        fourthImage: nikeBlazer4,
         itemID: 'item14',
         brand: 'Nike',
     },
@@ -147,6 +219,8 @@ export const info = [
         model: 'Nike Giannis Immortality 2',
         image: nikeGiannis,
         secondImage: nikeGiannis2,
+        thirdImage: nikeGiannis3,
+        fourthImage: nikeGiannis4,
         itemID: 'item15',
         brand: 'Nike',
     },
